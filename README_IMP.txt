9c71c29fa40b7f19454ae802a3509e95790fe5ac
Jamshed Rabadi <jamshedrabadi@gmail.com>
2023-06-24 19:26:08 +0530

    EJS cleanup

--- README_IMP.txt
+++ README_IMP.txt
@@ -3,3 +3,7 @@ npm i express dotenv ejs morgan passport passport-local express-session
 npm i -D nodemon jsdoc cross-env
 
 npm i -D eslint eslint-config-semistandard eslint-config-standard eslint-plugin-import eslint-plugin-node eslint-plugin-promise eslint-plugin-sort eslint-plugin-sort-requires eslint-plugin-standard eslint-plugin-jsdoc
+
+-----
+
+1. get data from db (static now)
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,13 +3,15 @@ const passportConfig = require('../config/passport.js');
 const router = express.Router();
 const userController = require('../controllers/user.controller.js');
 
-// Views
+// ---------- Views ----------
+
 router.get('/', passportConfig.isAuthenticated, userController.loadLandingPage);
 router.get('/dashboard', passportConfig.isAuthenticated, userController.loadDashboardPage);
 router.get('/login', userController.loadLoginPage);
 router.get('/logout', userController.loadLogoutPage);
 
-// APIs
+// ---------- APIs ----------
+
 router.post('/login', userController.authenticateUser);
 
 module.exports = router;
